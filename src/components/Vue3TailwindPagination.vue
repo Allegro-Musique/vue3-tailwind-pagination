0307e4aaac53bf6fc424cbc91ecc919f45885515
MAREGA Oumar <oumarmarega18@gmail.com>
2022-08-30 17:22:31 +0200

    patch : breakpoint

--- src/components/Vue3TailwindPagination.vue
+++ src/components/Vue3TailwindPagination.vue
@@ -1,14 +1,5 @@
 <template>
   <div v-if="total_pages > 1" class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-    <div class="flex-1 flex justify-between sm:hidden">
-      <a href="#" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500">
-        Précédent
-      </a>
-      <a href="#" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500">
-        Suivant
-      </a>
-    </div>
-
     <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
       <span v-if="show_text">
         <p class="text-sm text-gray-700">
@@ -98,7 +89,7 @@ export default {
     },
     color: {
       type: String,
-      default: 'text-gray-700'
+      default: 'text-gray-500'
     },
     active_color: {
       type: String,
