0161b899e120b3a451ad78b896f421567eb4f309
Oumar MAREGA <oumarmarega18@gmail.com>
2025-02-24 15:24:30 +0100

    patch(breakpoint)

--- src/components/Vue3TailwindPagination.vue
+++ src/components/Vue3TailwindPagination.vue
@@ -1,6 +1,7 @@
 <template>
-  <div v-if="total_pages > 1" class="bg-white px-4 py-3 flex text-xs items-center justify-between sm:px-6">
-    <div class="flex-1 flex justify-between md:hidden block">
+  <div v-if="total_pages > 1" class="bg-white px-4 py-3 flex items-center justify-between sm:px-6">
+    <!-- Pagination pour les écrans mobiles -->
+    <div class="flex-1 flex justify-between md:hidden">
       <nav class="relative z-0 inline-flex rounded-md -space-x-px" aria-label="Pagination">
         <span>
           <a @click="changePage(1)"
@@ -46,7 +47,8 @@
       </nav>
     </div>
 
-    <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+    <!-- Pagination pour les écrans desktop -->
+    <div class="hidden md:flex-1 md:flex md:items-center md:justify-between">
       <span v-if="show_text">
         <p class="text-sm text-gray-700">
           {{ text.one }}
@@ -59,69 +61,68 @@
         </p>
       </span>
 
-      <div class="invisible md:visible">
-        <nav class="relative z-0 inline-flex rounded-md -space-x-px" aria-label="Pagination">
-          <span>
-            <a @click="changePage(1)"
-               :class="`relative mr-2 inline-flex rounded-full shadow-xs items-center px-2 py-2 border ${border_color} ${background} text-sm font-medium ${color}`">
-              <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 cursor-pointer">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"/>
-              </svg>
-            </a>
-          </span>
+      <nav class="relative z-0 inline-flex rounded-md -space-x-px" aria-label="Pagination">
+        <span>
+          <a @click="changePage(1)"
+             :class="`relative mr-2 inline-flex rounded-full shadow-xs items-center px-2 py-2 border ${border_color} ${background} text-sm font-medium ${color}`">
+            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 cursor-pointer">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"/>
+            </svg>
+          </a>
+        </span>
 
-          <span>
-            <a @click="changePage(current_page - 1)"
-               :class="`relative mr-2 inline-flex rounded-full shadow-xs items-center px-2 py-2 border ${border_color} ${background} text-sm font-medium ${color}`">
-              <span class="sr-only">Previous</span>
-              <svg class="h-5 w-5 cursor-pointer" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"/>
-              </svg>
-            </a>
-          </span>
+        <span>
+          <a @click="changePage(current_page - 1)"
+             :class="`relative mr-2 inline-flex rounded-full shadow-xs items-center px-2 py-2 border ${border_color} ${background} text-sm font-medium ${color}`">
+            <span class="sr-only">Previous</span>
+            <svg class="h-5 w-5 cursor-pointer" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"/>
+            </svg>
+          </a>
+        </span>
 
-          <span v-if="hasFirst()">
-            <a :class="`relative cursor-pointer inline-flex items-center rounded-full mr-2 shadow-xs px-4 py-2 border ${border_color} ${background} text-sm font-medium ${color}`"
-               @click.prevent="changePage(1)">
-              <span>1</span>
-            </a>
-          </span>
+        <span v-if="hasFirst()">
+          <a :class="`relative cursor-pointer inline-flex items-center rounded-full mr-2 shadow-xs px-4 py-2 border ${border_color} ${background} text-sm font-medium ${color}`"
+             @click.prevent="changePage(1)">
+            <span>1</span>
+          </a>
+        </span>
 
-          <a @click="less()"
-             class="relative inline-flex items-center cursor-pointer px-4 py-2 mr-2 ml-1 bg-white text-sm font-medium text-gray-700"
-             v-if="hasFirst()">...</a>
+        <a @click="less()"
+           class="relative inline-flex items-center cursor-pointer px-4 py-2 mr-2 ml-1 bg-white text-sm font-medium text-gray-700"
+           v-if="hasFirst()">...</a>
 
-          <span v-for="(page, index) in pages" :key="index">
-            <a :class="[current_page === page ? `${active_background} ${active_color} border ${active_border_color}` : `${background} border ${border_color}`]"
-               class="relative inline-flex items-center mr-2 px-4 py-2 rounded-full cursor-pointer shadow-xs text-sm font-medium"
-               @click.prevent="changePage(page)">
-              <span :class="[current_page === page ? 'text-white' : '']">{{ page }}</span>
-            </a>
-          </span>
+        <span v-for="(page, index) in pages" :key="index">
+          <a :class="[current_page === page ? `${active_background} ${active_color} border ${active_border_color}` : `${background} border ${border_color}`]"
+             class="relative inline-flex items-center mr-2 px-4 py-2 rounded-full cursor-pointer shadow-xs text-sm font-medium"
+             @click.prevent="changePage(page)">
+            <span :class="[current_page === page ? 'text-white' : '']">{{ page }}</span>
+          </a>
+        </span>
 
-          <a @click="more()"
-             class="relative inline-flex cursor-pointer mr-2 items-center px-4 py-2 text-sm font-medium text-gray-700"
-             v-if="showLast()">...</a>
-          <span>
-            <a @click="changePage(current_page + 1)"
-               :class="`relative inline-flex items-center mr-2 px-2 py-2 rounded-full shadow-xs bg-white text-sm font-medium border ${border_color} ${color} ${background}`">
-              <span class="sr-only">Next</span>
-              <svg class="h-5 w-5 cursor-pointer" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"/>
-              </svg>
-            </a>
-          </span>
+        <a @click="more()"
+           class="relative inline-flex cursor-pointer mr-2 items-center px-4 py-2 text-sm font-medium text-gray-700"
+           v-if="showLast()">...</a>
 
-          <span>
-            <a @click="changePage(total_pages)"
-               :class="`relative inline-flex items-center px-2 py-2 rounded-full shadow-xs text-sm font-medium border ${border_color} ${color} ${background}`">
-              <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 cursor-pointer">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"/>
-              </svg>
-            </a>
-          </span>
-        </nav>
-      </div>
+        <span>
+          <a @click="changePage(current_page + 1)"
+             :class="`relative inline-flex items-center mr-2 px-2 py-2 rounded-full shadow-xs bg-white text-sm font-medium border ${border_color} ${color} ${background}`">
+            <span class="sr-only">Next</span>
+            <svg class="h-5 w-5 cursor-pointer" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"/>
+            </svg>
+          </a>
+        </span>
+
+        <span>
+          <a @click="changePage(total_pages)"
+             :class="`relative inline-flex items-center px-2 py-2 rounded-full shadow-xs text-sm font-medium border ${border_color} ${color} ${background}`">
+            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 cursor-pointer">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"/>
+            </svg>
+          </a>
+        </span>
+      </nav>
     </div>
   </div>
 </template>
